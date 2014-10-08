b756482405627867207b13187e9af52200c04bdd
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2014-10-08 18:40:24 +0100

    Update Animation Pause

---
+++ Assets/Animation/AnimationTesting/Scripts/QuickPauseMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuickPauseMenu : MonoBehaviour {
+
+	bool isPaused;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("escape"))	isPaused = !isPaused;
+		if(isPaused)	Time.timeScale = 0;
+		else 	Time.timeScale = 1;
+		
+	}
+
+	void OnGUI(){
+		if(isPaused)
+			GUI.Box(new Rect(Screen.width /2 - 100,Screen.height /2 - 100, 250, 50), "Pause Menu");
+	}
+}
