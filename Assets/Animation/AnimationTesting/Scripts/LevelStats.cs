670f15c83265310b3ad3ec41e1ee38e0d34a2e2f
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2014-10-08 22:57:37 +0100

    Update Animation
    Level stats added health and stamina bar

--- Assets/Animation/AnimationTesting/Scripts/LevelStats.cs
+++ Assets/Animation/AnimationTesting/Scripts/LevelStats.cs
@@ -5,10 +5,16 @@ public class LevelStats : MonoBehaviour {
 
 	void OnGUI(){
 		GUILayout.Label ("Health: " + Player_Unitychan.playerHealth + "\n" + 
-		                 "Stamina: " + Player_Unitychan.playerStamina + "\n" + 
-		                 "isAttacking: " + Player_Unitychan.playerAnim.GetBool("isAttacking") + "\n" + 
-		                 "isSprintHeld: " + Player_Unitychan.playerAnim.GetBool("isSprintHeld") + "\n" + 
-		                 "isJumpPressed: " + Player_Unitychan.playerAnim.GetBool("isJumpPressed") + "\n" + 
-		                 "isDamaged: " + Player_Unitychan.playerAnim.GetBool("isDamaged") + "\n");
+		                 "Stamina: " + Player_Unitychan.playerStamina);
+
+		GUI.Box(new Rect(0, 30, 500, 20), "Health");
+		GUI.Box(new Rect(0, 30, Player_Unitychan.playerHealth/2, 20), Player_Unitychan.playerHealth.ToString());
+
+		GUI.Box(new Rect(0, 50, 500, 20), "Stamina");
+		GUI.Box(new Rect(0, 50, Player_Unitychan.playerStamina/2, 20), Player_Unitychan.playerStamina.ToString());
+
+		if(Player_Unitychan.playerHealth <= 0){
+			GUI.Box(new Rect(0, 100, Screen.width, 300), "YOU DIED");
+		}
 	}
 }
