51ba7b08b09b0305d84cbfd250260cc0798aac5b
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2014-10-10 20:17:15 +0100

    Update Animation

--- Assets/Animation/AnimationTesting/Scripts/Player_Unitychan.cs
+++ Assets/Animation/AnimationTesting/Scripts/Player_Unitychan.cs
@@ -5,8 +5,9 @@ public class Player_Unitychan : MonoBehaviour {
 
 	// Player
 	public static Animator playerAnim;
-	public static int playerHealth = 1000;
-	public static int playerStamina = 1000;
+	public static float playerHealth = 1000;
+	public static float playerStamina = 1000;
+	float staminaRegen = 0.5f;
 	public static bool isPlayerHit;
 	float forwardSpeed = 1.5f;
 	float backwardSpeed = 1.0f;
@@ -23,7 +24,13 @@ public class Player_Unitychan : MonoBehaviour {
 	int attackHash = Animator.StringToHash ("Base Layer.Attack");
 	int idleHash = Animator.StringToHash ("Base Layer.Idle");
 	int walkHash = Animator.StringToHash ("Base Layer.Walk2");
+	int walkLeftHash = Animator.StringToHash ("Base Layer.Walk3");
+	int walkRightHash = Animator.StringToHash ("Base Layer.Walk4");
 	int runHash = Animator.StringToHash ("Base Layer.Run1");
+	int runLeftHash = Animator.StringToHash ("Base Layer.Run2");
+	int runRightHash = Animator.StringToHash ("Base Layer.Run3");
+	int runHandHash = Animator.StringToHash ("Base Layer.RunHand");
+	int slideHash = Animator.StringToHash ("Base Layer.Slide");
 	int damaged1Hash = Animator.StringToHash ("Base Layer.Damaged1");
 	int damaged2Hash = Animator.StringToHash ("Base Layer.Damaged2");
 	int exaustHash = Animator.StringToHash ("Base Layer.Exaust");
@@ -41,7 +48,7 @@ public class Player_Unitychan : MonoBehaviour {
 		idleAnimTime++;
 
 		// Stamina regen
-		playerAnim.SetInteger("Stamina", playerStamina);
+		playerAnim.SetFloat("Stamina", playerStamina);
 		if(playerStamina <= 0){
 			playerAnim.SetBool("isSprintHeld", false);
 			canAttack = false;
@@ -53,8 +60,10 @@ public class Player_Unitychan : MonoBehaviour {
 		if(playerStamina < 1000){
 			if(playerAnimHash == idleHash || 
 			   playerAnimHash == walkHash || 
+			   playerAnimHash == walkLeftHash || 
+			   playerAnimHash == walkRightHash || 
 			   playerAnimHash == exaustHash)
-				playerStamina++;
+				playerStamina += staminaRegen;
 		}
 		if(idleAnimTime > 1000){
 			idleAnimTime = 0;
@@ -64,8 +73,8 @@ public class Player_Unitychan : MonoBehaviour {
 		if(isPlayerHit){
 			playerAnim.SetBool("isDamaged", true);
 			isPlayerHit = false;
-			playerHealth -= 50;
-			playerAnim.SetInteger("Health", playerHealth);
+			playerHealth -= 150;
+			playerAnim.SetFloat("Health", playerHealth);
 		}
 		else{
 			playerAnim.SetBool("isDamaged", false);
@@ -96,11 +105,8 @@ public class Player_Unitychan : MonoBehaviour {
 			//print ("Player is attacking");
 			canMove = false;
         }
-        
-        
-        
-        
-		
+
+
 		// Player jump
 		if(Input.GetButton("Jump")){
 			playerAnim.SetBool("isJumpPressed", true);
@@ -108,7 +114,7 @@ public class Player_Unitychan : MonoBehaviour {
 		if(Input.GetButtonDown("Jump")){
 			playerAnim.SetBool("isJumpPressed", true);
 			playerStamina -= 50;
-			playerAnim.SetInteger("Stamina", playerStamina);
+			playerAnim.SetFloat("Stamina", playerStamina);
 		}
 		if(Input.GetButtonUp("Jump")) {
 			playerAnim.SetBool("isJumpPressed", false);
@@ -120,7 +126,7 @@ public class Player_Unitychan : MonoBehaviour {
 			   playerAnim.GetBool("isAttacking") == false &&
 			   playerAnimHash != attackHash){
 				playerStamina -= 250;
-				playerAnim.SetInteger("Stamina", 50);
+				playerAnim.SetFloat("Stamina", 50);
 				playerAnim.SetBool("isAttacking", true);
 				canAttack = false;
 
@@ -138,18 +144,66 @@ public class Player_Unitychan : MonoBehaviour {
 		else    
 			playerAnim.SetBool("isSprintHeld", false);
 
+		/*
+		isSlidePressed
+		Slide
+
+		isRunLeft	Run2
+		isRunRight	Run3
+		isLookLeft	Walk3
+		isLookRight	Walk4
+		isRunHandPressed RunHand
+		*/
+		
+		// Player walking and running animations
+		// Slide
+		if(Input.GetButtonDown("Fire1"))		playerAnim.SetBool("isSlidePressed", true);
+		else						playerAnim.SetBool("isSlidePressed", false);
+		// Run hand
+		if(Input.GetButtonDown("Fire1"))		playerAnim.SetBool("isRunHandPressed", true);
+		else						playerAnim.SetBool("isRunHandPressed", false);
+
+		// Walk left
+		if(Input.GetKey(KeyCode.A) && !playerAnim.GetBool("isSprintHeld"))		
+		   	playerAnim.SetBool("isLookLeft", true);
+		else						
+			playerAnim.SetBool("isLookLeft", false);
+		// Walk right
+		if(Input.GetKey(KeyCode.D) && !playerAnim.GetBool("isSprintHeld"))		
+			playerAnim.SetBool("isLookRight", true);
+		else						
+			playerAnim.SetBool("isLookRight", false);
+
+		// Run left
+		if(Input.GetKey(KeyCode.A) && playerAnim.GetBool("isSprintHeld"))		
+			playerAnim.SetBool("isRunLeft", true);
+		else						
+			playerAnim.SetBool("isRunLeft", false);
+		// Run right
+		if(Input.GetKey(KeyCode.D) && playerAnim.GetBool("isSprintHeld"))		
+			playerAnim.SetBool("isRunRight", true);
+		else						
+			playerAnim.SetBool("isRunRight", false);
+
+
+
+
 		// Sprinting moves player faster
 		// Sprinting reduces stamina
-		if(playerAnimHash == runHash){
+		if(playerAnimHash == runHash ||
+		   playerAnimHash == runLeftHash ||
+		   playerAnimHash == runRightHash ||
+		   playerAnimHash == runHandHash){
 			//print ("player running");
 			forwardSpeed = 4.5f;
-			playerStamina--;
+			playerStamina -= staminaRegen;
 		}
 		else
 			forwardSpeed = 1.5f;
 
 		// Run and jump speed??
-		if(playerAnimHash == hopHash)
+		if(playerAnimHash == hopHash ||
+		   playerAnimHash == slideHash)
 			forwardSpeed = 4.5f;
 	}
 
